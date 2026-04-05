9ed9c35d4d1b4e9a20e6d7628957fa11a9a66bb9
Josmeyly Yamil Duarte Amparo <josmeyly_duarte@ucne.edu.do>
2026-04-05 00:43:18 -0400

    Implementar utilidad para sincronización de datos Red/Local.

---
+++ app/src/main/java/com/example/pawcare/domain/util/NetworkBoundResource.kt
@@ -0,0 +1,26 @@
+package com.example.pawcare.domain.util
+
+import kotlinx.coroutines.flow.*
+
+inline fun <ResultType, RequestType> networkBoundResource(
+    crossinline query: () -> Flow<ResultType>,
+    crossinline fetch: suspend () -> RequestType,
+    crossinline saveFetchResult: suspend (RequestType) -> Unit,
+    crossinline shouldFetch: (ResultType) -> Boolean = { true }
+) = flow {
+    val data = query().first()
+
+    val flow = if (shouldFetch(data)) {
+        emit(Resource.Loading<ResultType>())
+        try {
+            saveFetchResult(fetch())
+            query().map { Resource.Success(it) }
+        } catch (throwable: Throwable) {
+            query().map { Resource.Error(throwable.message ?: "Unknown Error", data) }
+        }
+    } else {
+        query().map { Resource.Success(data) }
+    }
+
+    emitAll(flow)
+}
