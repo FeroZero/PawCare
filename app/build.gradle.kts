e5fce5a256a8417c38aea3e62e2341f0e4c05ccd
Josmeyly Yamil Duarte Amparo <josmeyly_duarte@ucne.edu.do>
2026-04-08 00:07:38 -0400

    Configurar dependencia de iconos extendidos de Material Design.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.material.icons.extended)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.activity.compose)
 
