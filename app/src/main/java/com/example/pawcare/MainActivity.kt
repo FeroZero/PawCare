52b75cdaf51db443512158014f1259d4f0170688
Josmeyly Yamil Duarte Amparo <josmeyly_duarte@ucne.edu.do>
2026-04-08 00:10:06 -0400

    Configurar punto de entrada con NavController e inyección de dependencias.

--- app/src/main/java/com/example/pawcare/MainActivity.kt
+++ app/src/main/java/com/example/pawcare/MainActivity.kt
@@ -5,18 +5,30 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.compose.rememberNavController
+import com.example.pawcare.domain.repository.PetRepository
+import com.example.pawcare.presentation.navigation.NavGraph
 import com.example.pawcare.ui.theme.PawCareTheme
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    
+    @Inject
+    lateinit var petRepository: PetRepository
+
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             PawCareTheme {
-                // TODO: Setup Navigation
+                val navController = rememberNavController()
+                NavGraph(
+                    navController = navController,
+                    petRepository = petRepository
+                )
             }
         }
     }
